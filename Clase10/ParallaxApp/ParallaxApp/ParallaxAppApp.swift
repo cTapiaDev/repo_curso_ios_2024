20f6dd550540ab03130d728ad28c0343154e6958
ctapiadev <ctapiadev@gmail.com>
2024-12-06 22:49:49 -0300

    clase 10

---
+++ Clase10/ParallaxApp/ParallaxApp/ParallaxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxAppApp.swift
+//  ParallaxApp
+//
+//  Created by Cony Pierola on 06-12-24.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
